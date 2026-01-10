78e844db061ffa9f2bae1fefc27ed2c16c18ae69
davidyassa <davidyassa@hotmail.com>
2026-01-11 00:42:11 +0200

    Observer Behavioural Pattern

---
+++ src/Behavioural/Observer/Subject.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Behavioural.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author DELL 7550
+ */
+public class Subject {
+
+    private final List<Observer> observers = new ArrayList<>();
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
