76323117e74e1ac1213eb04417eccc3d665bf339
davidyassa <davidyassa@hotmail.com>
2026-01-11 02:18:03 +0200

    State Behavioral Pattern
    Optional in Term 5 Fall 2025

---
+++ src/Behavioural/State/RedLightState.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Behavioural.State;
+
+/**
+ *
+ * @author DELL 7550
+ */
+public class RedLightState implements State {
+
+    @Override
+    public void doAction(Context context) {
+        System.out.println("Red Light - Stop");
+        context.setState(new GreenLightState());
+
+    }
+
+    @Override
+    public String getName() {
+        return "RED";
+    }
+}
